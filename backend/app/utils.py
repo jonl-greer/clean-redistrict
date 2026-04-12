3686dc02d0cdd4e7c7c20d1dedd469140b826a81
jonl-greer <jlgreer@uchicago.edu>
2026-04-12 16:40:43 -0500

    Update utils.py

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -4,3 +4,9 @@ from pathlib import Path
 import zipfile
 import tempfile
 import os
+
+def load_shapefile():
+  """
+  Accepts zipfile, extracts shapefiles, loads shapefiles with geopandas, reproject to common geography (EPSG:4269), returns GeoDataFrame
+  """
+  return
